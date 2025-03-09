fe7a439821fd6b8181a28a45a4f32ed8d53eabe4
syahlasandiani <ssandianiaa@gmail.com>
2025-03-09 20:37:46 +0700

    Create AngkaSialException.java

---
+++ Prak 3/AngkaSialException.java
@@ -0,0 +1,11 @@
+/*  Nama File   : AngkaSialException.java
+ *  Deskripsi   : Program penggunaan eksepsi menggunakan class library Java
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 06 Maret 2025
+*/
+
+public class AngkaSialException extends Exception{
+   public AngkaSialException(){
+    super("jangan memasukan angka 13 karena angka sial !!!");
+   } 
+}

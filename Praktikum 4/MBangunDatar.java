c45b3fe2829129f238c23ec864f6d80fa2778744
syahlasandiani <ssandianiaa@gmail.com>
2025-03-24 16:33:16 +0700

    Create MBangunDatar.java

---
+++ Praktikum 4/MBangunDatar.java
@@ -0,0 +1,23 @@
+public class MBangunDatar {
+    public static void main(String[] args) {
+        BangunDatar bangunDatar = new BangunDatar();
+        bangunDatar.setJmlSisi(4);
+        bangunDatar.setWarna("Biru");
+        bangunDatar.setBorder("Hitam");
+        System.out.println("Bangun Datar:");
+        bangunDatar.printInfo();
+
+        Persegi persegi = new Persegi(5, "Kuning", "Merah");
+        System.out.println("\nPersegi:");
+        System.out.println("Sisi: " + persegi.getSisi());
+        System.out.println("Luas: " + persegi.getLuas());
+        System.out.println("Keliling: " + persegi.getKeliling());
+        System.out.println("Diagonal: " + persegi.getDiagonal());
+
+        Lingkaran lingkaran = new Lingkaran(14, "Hijau", "Putih");
+        System.out.println("\nLingkaran:");
+        System.out.println("Jari-jari: " + lingkaran.getJari());
+        System.out.println("Luas: " + lingkaran.getLuas());
+        System.out.println("Keliling: " + lingkaran.getKeliling());
+    }
+}

6cc404d988f060b450949301b21ec4164a783b05
syahlasandiani <ssandianiaa@gmail.com>
2025-03-30 10:24:10 +0700

    Create IResize.java

---
+++ Praktikum 5/IResize.java
@@ -0,0 +1,10 @@
+/*  Nama File   : IResize.java
+ *  Deskripsi   : Interface untuk class BangunDatar
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 20 Maret 2025 */
+
+public interface IResize {
+    public void zoomIn();
+    public void zoomOut();
+    public void zoom(int percent);
+}

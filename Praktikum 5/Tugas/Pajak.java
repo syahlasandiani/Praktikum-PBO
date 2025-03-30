ea24e3cbfb0267542d58f60ffed52be9cfa1fce4
syahlasandiani <ssandianiaa@gmail.com>
2025-03-30 11:21:33 +0700

    Create Pajak.java

---
+++ Praktikum 5/Tugas/Pajak.java
@@ -0,0 +1,10 @@
+/*  Nama File   : Pajak.java
+ *  Deskripsi   : Interface Pajak
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 23 Maret 2025 */
+
+package Tugas;
+
+interface Pajak {
+    double hitungPajak();
+}

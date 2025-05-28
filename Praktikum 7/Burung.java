a5ae77e50fe33fe0df908a96ba8ffa8e3a106851
syahlasandiani <ssandianiaa@gmail.com>
2025-05-28 07:08:06 +0700

    Create Burung.java

---
+++ Praktikum 7/Burung.java
@@ -0,0 +1,13 @@
+/*  Nama File   : Burung.java
+ *  Deskripsi   : Class Burung
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 8 Mei 2025 */
+
+public class Burung extends Anabul {
+    public void suara() {
+        System.out.println("Cuit");
+    }
+    public void gerak() {
+        System.out.println("Burung terbang.");
+    }
+}

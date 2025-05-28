f3ed6980ea6a5f245cdbe1b5eecb9eae4614ddef
syahlasandiani <ssandianiaa@gmail.com>
2025-05-28 07:08:27 +0700

    Create Datum.java

---
+++ Praktikum 7/Datum.java
@@ -0,0 +1,16 @@
+/*  Nama File   : Datum.java
+ *  Deskripsi   : Class Datum
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 8 Mei 2025 */
+
+public class Datum<T> {
+    private T isi;
+
+    public T getIsi() {
+        return isi;
+    }
+
+    public void setIsi(T isi) {
+        this.isi = isi;
+    }
+}

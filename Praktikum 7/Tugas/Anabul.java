1568cbb973ca0274193c5615c2a122e8e755bb98
syahlasandiani <ssandianiaa@gmail.com>
2025-05-28 07:19:37 +0700

    Create Anabul.java

---
+++ Praktikum 7/Tugas/Anabul.java
@@ -0,0 +1,9 @@
+/*  Nama File   : Anabul.java
+ *  Deskripsi   : Class Anabul
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 8 Mei 2025 */
+
+public abstract class Anabul {
+    public abstract void suara();
+    public abstract void gerak();
+}

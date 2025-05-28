461ecb1c3996b8b50346e2d5319ed4cfd6fad0fe
syahlasandiani <ssandianiaa@gmail.com>
2025-05-28 07:08:48 +0700

    Create Kucing.java

---
+++ Praktikum 7/Kucing.java
@@ -0,0 +1,13 @@
+/*  Nama File   : Kucing.java
+ *  Deskripsi   : Class Kucing
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 8 Mei 2025 */
+
+public class Kucing extends Anabul {
+    public void suara() {
+        System.out.println("Meong");
+    }
+    public void gerak() {
+        System.out.println("Kucing melata.");
+    }
+}

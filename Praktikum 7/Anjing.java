253f60a303a13073d6d81a54b65936e86d00df8b
syahlasandiani <ssandianiaa@gmail.com>
2025-05-28 07:07:40 +0700

    Create Anjing.java

---
+++ Praktikum 7/Anjing.java
@@ -0,0 +1,13 @@
+/*  Nama File   : Anjing.java
+ *  Deskripsi   : Class Anjing
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 8 Mei 2025 */
+
+public class Anjing extends Anabul {
+    public void suara() {
+        System.out.println("Guk-guk");
+    }
+    public void gerak() {
+        System.out.println("Anjing melata.");
+    }
+}

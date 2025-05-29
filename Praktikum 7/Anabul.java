900a0345803596b96dbf2d78504e4f2f65ad1a68
syahlasandiani <ssandianiaa@gmail.com>
2025-05-29 09:34:13 +0700

    Delete Praktikum 7/Anabul.java

--- Praktikum 7/Anabul.java
+++
@@ -1,9 +0,0 @@
-/*  Nama File   : Anabul.java
- *  Deskripsi   : Class Anabul
- *  Pembuat     : Syahla Sandiani / 24060123130066
- *  Tanggal     : 8 Mei 2025 */
-
-public abstract class Anabul {
-    public abstract void suara();
-    public abstract void gerak();
-}

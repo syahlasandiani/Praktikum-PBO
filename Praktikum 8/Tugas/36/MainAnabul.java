d88d2412e6f1635d444ca4624c6708e355fd096c
syahlasandiani <ssandianiaa@gmail.com>
2025-05-30 14:09:07 +0700

    Create MainAnabul.java

---
+++ Praktikum 8/Tugas/36/MainAnabul.java
@@ -0,0 +1,13 @@
+package Tugas36;
+
+/*  Nama File   : MainAnabul.java
+ *  Deskripsi   : Class Main Anabul
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 10 Mei 2025 */
+
+public class MainAnabul {
+    public static void main(String[] args) {
+        KoleksiAnabul koleksi = new KoleksiAnabul();
+        koleksi.showAll();
+    }
+}

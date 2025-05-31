64a0b35bc459743fff238ef4ee735a7f3da12211
syahlasandiani <ssandianiaa@gmail.com>
2025-05-31 10:25:08 +0700

    Create PersonDAO.java

---
+++ Praktikum 9/PersonDAO.java
@@ -0,0 +1,8 @@
+/*  Nama File   : PersonDAO.java
+ *  Deskripsi   : interface untuk person access object
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 15 Mei 2025 */
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}

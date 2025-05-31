ed4003b7c5663b39989cfc1fa1b4db511d1d6fb4
syahlasandiani <ssandianiaa@gmail.com>
2025-05-31 10:25:28 +0700

    Create ReadSerializedPerson.java

---
+++ Praktikum 9/ReadSerializedPerson.java
@@ -0,0 +1,21 @@
+/*  Nama File   : ReadSerializedPerson.java
+ *  Deskripsi   : Program untuk deserialisasi objek Person
+ *  Pembuat     : Syahla Sandiani / 24060123130066
+ *  Tanggal     : 15 Mei 2025 */
+
+import java.io.*;
+
+public class ReadSerializedPerson {
+    public static void main(String[] args) {
+        Person person = null;
+        try {
+            FileInputStream f = new FileInputStream("person.ser");
+            ObjectInputStream s = new ObjectInputStream(f);
+            person = (Person) s.readObject();
+            s.close();
+            System.out.println("serialized person name = " + person.getName());
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
